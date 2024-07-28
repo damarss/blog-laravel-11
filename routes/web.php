8e286e15516ed4b2c88b3e9f8bff3e6afcbcc93e
damarsepti627 <damarsepti627@gmail.com>
2024-07-28 14:31:29 +0000

    alter table user add username

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
-Route::get('/authors/{user}', function (User $user) {
-    return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
+Route::get('/authors/{user:username}', function (User $user) {
+    return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', ['title' => 'Articles in ' . $category->name, 'posts' => $category->posts]);
+    return view('posts', ['title' => count($category->posts) . ' Articles in ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
