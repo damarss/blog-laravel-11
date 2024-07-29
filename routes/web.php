01f78a8a6d4f9f355685a5851e7e905eac67f8bf
damarsepti627 <damarsepti627@gmail.com>
2024-07-29 06:27:03 +0000

    use eager load to prevent n+1 problem

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog Page', 'posts' => Post::all()]);
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+
+    return view('posts', ['title' => 'Blog Page', 'posts' => Post::latest()->get()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -22,10 +24,14 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load(['author', 'category']);
+
     return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load(['author', 'category']);
+
     return view('posts', ['title' => count($category->posts) . ' Articles in ' . $category->name, 'posts' => $category->posts]);
 });
 
